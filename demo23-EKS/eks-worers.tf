ac5e739d62392ff44811670c927b5278cb9e8964
andrii Perun <perandr@gmail.com>
2019-06-12 00:48:43 +0300

    Fixed output for kubernetes config and configmap. Made some changes in workers.

--- demo23-EKS/eks-worers.tf
+++ demo23-EKS/eks-worers.tf
@@ -1,12 +1,21 @@
 data "aws_ami" "eks_worker" {
+    # filter {
+    #     name = "name"
+    #     values = ["*eks-node-*"]
+    # }
+
+    # most_recent = true
+    # owners = ["602401143452"] # Amazon
+
     filter {
-        name = "name"
-        values = ["*eks-node-*"]
+      name   = "name"
+      values = ["amazon-eks-node-${aws_eks_cluster.perandr_kubernetes.version}-v*"]
     }
 
     most_recent = true
-    owners = ["602401143452"] # Amazon
+    owners      = ["602401143452"] # Amazon EKS AMI Account ID
 }
+
 output "eks_worker-AMI" {
   value = "${data.aws_ami.eks_worker.image_id}"
 }
@@ -20,11 +29,12 @@ set -o xtrace
 USERDATA
 }
 
+
 resource "aws_launch_configuration" "eks_demo" {
   associate_public_ip_address = true
   iam_instance_profile = "${aws_iam_instance_profile.perandr_eks_node.name}"
   image_id = "${data.aws_ami.eks_worker.image_id}"
-  instance_type = "t2.medium"
+  instance_type = "t2.small"
   name_prefix = "terraform_eks_perandr"
   security_groups = ["${aws_security_group.eks_cluster_node.id}"]
 
--- demo23-EKS/outputs.tf
+++ demo23-EKS/outputs.tf
@@ -1,18 +1,16 @@
 locals {
   kubeconfig = <<KUBCONFIG
-
-
 apiVersion: v1
 clusters:
 - cluster:
-    server:${aws_eks_cluster.perandr_kubernetes.endpoint}
+    server: ${aws_eks_cluster.perandr_kubernetes.endpoint}
     certificate-authority-data: ${aws_eks_cluster.perandr_kubernetes.certificate_authority.0.data}
   name: kubernetes
 contexts:
 - context:
      cluster: kubernetes
-     user:aws
-   name:aws
+     user: aws
+  name: aws
 current-context: aws
 kind: Config
 preferences: {}
@@ -21,12 +19,11 @@ users:
   user:
     exec:
        apiVersion: client.authentication.k8s.io/v1alpha1
-       command: heptio-authenticator-aws
+       command: aws-iam-authenticator
        args:
           - "token"
           - "-i"
           - "${var.eks_cluster_name}"
-
 KUBCONFIG
 }
 
@@ -39,17 +36,17 @@ locals {
 apiVersion: v1
 kind: ConfigMap
 metadata:
-   name: aws-auth
-   namespace: kube-system
+  name: aws-auth
+  namespace: kube-system
 data:
-    mapRoles: |
+  mapRoles:
         - rolearn: ${aws_iam_role.eks_node_role.arn}
           username: system:node:{{EC2PrivateDNSName}}
           groups:
-              - system:bootstrappers
-              - system:nodes 
+              - system: bootstrappers
+              - system: nodes 
 CONFIGMAPAWSAUTH
 }
 output "config-map-aws-auth" {
-  value = "${local.kubeconfig}"
+  value = "${local.config-map-aws-auth}"
 }
